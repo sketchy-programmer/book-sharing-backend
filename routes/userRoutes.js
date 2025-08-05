483d33d70ba7933673576359f850b31b65b6e193
sketchy-programmer <sparamvirsingh7654321@gmail.com>
2025-08-05 19:05:40 -0400

    add new router for login & register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,12 @@ router.get('/', (req, res) => {
 router.get('/me', protect, (req, res) => {
     res.status(200).json({ message: 'User authenticated successfully' });
 });
+router.post('/register', (req, res) => {
+    res.status(201).json({ message: 'User registered successfully' });
+})
+router.post('/login', (req, res) => {
+    res.status(200).json({ message: 'User logged in successfully' })
+});
 router.get('/profile', protect, getProfile);
 router.post('/follow/:id', protect, followWriter);
 router.post('/favorite/:id', protect, addFavorite);
